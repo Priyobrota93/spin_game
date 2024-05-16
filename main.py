a136a22cdb989fbbc24b3bbda2a67396d52a13af
Priyobrota <luiz.khim962gmail.com>
2024-05-16 14:31:51 +0600

    spin and random number pick perfectly use any key and after show modal confetti use

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import psycopg2
-import random
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-DB_NAME = 'attend'
+DB_NAME = 'raffle_draw'
 DB_USER = 'openpg'
 DB_PASSWORD = 'openpgpwd'
 DB_HOST = 'localhost'
@@ -20,33 +19,45 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
+
 last_3_ids = []
-def fetch_random_employee_id():
+def fetch_random_draw_sl():
     global last_3_ids
     if last_3_ids:
-        cur.execute("SELECT employee_id FROM \"user\" WHERE employee_id NOT IN %s ORDER BY RANDOM() LIMIT 1", (tuple(last_3_ids),))
+        cur.execute("SELECT draw_sl FROM \"card1\" WHERE draw_sl NOT IN %s ORDER BY RANDOM() LIMIT 1", (tuple(last_3_ids),))
     else:
-        cur.execute("SELECT employee_id FROM \"user\" ORDER BY RANDOM() LIMIT 1")
-    employee_id = cur.fetchone()[0]
+        cur.execute("SELECT draw_sl FROM \"card1\" ORDER BY RANDOM() LIMIT 1")
+    draw_sl = cur.fetchone()[0]
 
     # Update
-    last_3_ids.append(employee_id)
-    if len(last_3_ids) > 3:
+    last_3_ids.append(draw_sl)
+    print(last_3_ids)
+    if len(last_3_ids) > 10:
         last_3_ids.pop(0)
 
-    return employee_id
+    return draw_sl
+
+from flask import render_template, jsonify
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    try:
+        # Fetch 3-digit integer values from the database
+        cur.execute("SELECT draw_sl FROM card1 ORDER BY draw_sl")
+        draw_sl_values = [str(row[0]).zfill(3) for row in cur.fetchall()]
+        
+        # Render the HTML template with the values
+        return render_template('index.html', draw_sl_values=draw_sl_values)
+    except Exception as e:
+        # Handle any exceptions that may occur during database access
+        return render_template('error.html', error=str(e))
 
 @app.route('/spin', methods=['GET'])
 def spin():
     try:
-        employee_id = fetch_random_employee_id()
-        digits = [int(digit) for digit in str(employee_id)]
-        return jsonify(success=True, digits=digits, employee_id=employee_id)
+        draw_sl = fetch_random_draw_sl()
+        digits = [int(digit) for digit in str(draw_sl)]
+        return jsonify(success=True, digits=digits, draw_sl=draw_sl)
     
     except Exception as e:
         return jsonify(success=False, error=str(e))
